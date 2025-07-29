5879828624881727815921da0a6db3e75a94a750
yofujitsu <yofujitsuuu@gmail.com>
2025-07-30 00:32:02 +0300

    master: init commit

---
+++ src/main/java/ru/it_one/yofujitsu/card_payment/CardPaymentApplication.java
@@ -0,0 +1,13 @@
+package ru.it_one.yofujitsu.card_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardPaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/it_one/yofujitsu/card_payment/CardPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.it_one.yofujitsu.card_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
